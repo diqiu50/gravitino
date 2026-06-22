194ab6b3c2bff6a2aa1e72f29bb327abadb760d5
yuhui <hui@datastrato.com>
2026-06-22 18:06:46 +0800

    Fix glue testset 00005_catalog: add required warehouse property

--- trino-connector/integration-test/src/test/resources/trino-ci-testset/testsets/glue/00005_catalog.sql
+++ trino-connector/integration-test/src/test/resources/trino-ci-testset/testsets/glue/00005_catalog.sql
@@ -2,8 +2,8 @@ call gravitino.system.create_catalog(
     'gt_glue_xxx1',
     'glue',
     map(
-        array['aws-region', 'aws-access-key-id', 'aws-secret-access-key', 'aws-glue-endpoint'],
-        array['${aws_region}', '${aws_access_key}', '${aws_secret_key}', '${?aws_glue_endpoint}']
+        array['aws-region', 'aws-access-key-id', 'aws-secret-access-key', 'warehouse'],
+        array['${aws_region}', '${aws_access_key}', '${aws_secret_key}', '${?warehouse}']
     )
 );
 
@@ -17,8 +17,8 @@ call gravitino.system.create_catalog(
     'gt_glue_xxx1',
     'glue',
     map(
-        array['aws-region', 'aws-access-key-id', 'aws-secret-access-key', 'aws-glue-endpoint'],
-        array['${aws_region}', '${aws_access_key}', '${aws_secret_key}', '${?aws_glue_endpoint}']
+        array['aws-region', 'aws-access-key-id', 'aws-secret-access-key', 'warehouse'],
+        array['${aws_region}', '${aws_access_key}', '${aws_secret_key}', '${?warehouse}']
     )
 );
 
